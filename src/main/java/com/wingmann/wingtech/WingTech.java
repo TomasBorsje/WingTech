87a34b83f0855827e916c9feb1416be9ecb61afe
Wingmann <shamanwingman@gmail.com>
2021-02-26 18:15:38 +1300

    Setting up git

--- src/main/java/com/wingmann/wingtech/WingTech.java
+++ src/main/java/com/wingmann/wingtech/WingTech.java
@@ -30,7 +30,6 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(WingTech.MODID)
 public class WingTech
 {
@@ -45,7 +44,7 @@ public class WingTech
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
-        MinecraftForge.EVENT_BUS.addListener(EventPriority.HIGH, WorldGen::generateChunk);
+        MinecraftForge.EVENT_BUS.addListener(EventPriority.HIGH, WorldGen::generateChunk); // World gen listener
     }
 
     private void setup(final FMLCommonSetupEvent event)
