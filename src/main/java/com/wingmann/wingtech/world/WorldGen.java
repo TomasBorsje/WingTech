5592869de3325c8173f8113889fc5d21960840dd
Wingmann <shamanwingman@gmail.com>
2021-06-27 02:46:23 +1200

    Fix flower world gen

--- src/main/java/com/wingmann/wingtech/world/WorldGen.java
+++ src/main/java/com/wingmann/wingtech/world/WorldGen.java
@@ -13,10 +13,8 @@ public class WorldGen {
 
     public static void generateChunk(final BiomeLoadingEvent event)
     {
-        // Generate Palladium Ore between y=48 and y=0
         ConfiguredFeature<?, ?> ORE_TUNGSTEN = Feature.ORE.configured(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, ModBlocks.TUNGSTEN_ORE.get().defaultBlockState(), 9)).range(64).squared().count(10);
-
-        ConfiguredFeature<?, ?> FLOWER_GEN = Feature.FLOWER.configured(FLOWER_CONFIG).decorated(Features.Placements.ADD_32).count(10);
+        ConfiguredFeature<?, ?> FLOWER_GEN = Feature.FLOWER.configured(FLOWER_CONFIG).decorated(Features.Placements.ADD_32).decorated(Features.Placements.HEIGHTMAP_SQUARE).count(1);
 
         // Generate ores
         event.getGeneration().addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, ORE_TUNGSTEN);
